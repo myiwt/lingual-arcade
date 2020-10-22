64d0d3a620518da1354471b63783a52ede17786f
ywtm <ywtm@protonmail.com>
2020-10-22 22:32:31 +1300

    Update code documentation

--- src/test/java/game/TestQuizAndScoreboard.java
+++ src/test/java/game/TestQuizAndScoreboard.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package game;
 
 import java.sql.Connection;
@@ -21,7 +16,7 @@ import org.junit.Test;
 import static org.junit.Assert.*;
 
 /**
- *
+ * Contains 5 unit test cases
  * @author Megan
  */
 public class TestQuizAndScoreboard {
